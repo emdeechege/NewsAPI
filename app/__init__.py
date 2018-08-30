0501ddf7af759d9581bc58786360d3c8b526a633
Tom Chege <emdeechege@gmail.com>
2018-08-30 09:59:29 +0300

    sources html url and api key added

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from .config import DevConfig
 
 #app initialization
-app= Flask(__name__)
+app= Flask(__name__,instance_relative_config = True)
 
 #set up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 
 
--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_SOURCE_URL='https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
 
 
 
