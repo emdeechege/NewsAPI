af531d95ea43881c7389c4575092c6ffe09e3502
Tom Chege <emdeechege@gmail.com>
2018-08-30 09:30:30 +0300

    index now viweable

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from app import app
+
+#our views
+@app.route('/')
+def index():
+    '''
+    Root function returning index/home page with data
+    '''
+    return render_template('index.html')
