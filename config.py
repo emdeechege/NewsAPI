2ece4c2285a520547449e36431f6751e4e88edf2
Tom Chege <emdeechege@gmail.com>
2018-08-31 17:58:35 +0300

    switched config py to main folder

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+ import os
+
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
     CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
+    API_KEY=os.environ.get('API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 
@@ -26,3 +30,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
